ce69f67d9ef0af0aa9c60385ab305c7c2cc12ae2
Prabeen04 <prabeen.strange@gmail.com>
2018-03-29 20:14:39 +0530

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
+import {  BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import store from './store';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const theme = createMuiTheme();
+
 ReactDOM.render(
     <Provider store={store}>
-        <MuiThemeProvider>
+        <MuiThemeProvider theme={theme}>
             <Router basename={process.env.PUBLIC_URL}>
                 <App />
             </Router>
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+
+import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form';
+
+export const rootReducer = combineReducers({
+    form: FormReducer
+})
+
+export default rootReducer;
