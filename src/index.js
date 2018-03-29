66795a0f3d186220a5dd7a3b3177a7e2b098b4f0
Prabeen04 <prabeen.strange@gmail.com>
2018-03-29 20:08:21 +0530

    redux store react router configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import store from './store';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <Router basename={process.env.PUBLIC_URL}>
+                <App />
+            </Router>
+        </MuiThemeProvider>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+// import { rootReducer } from '../reducers/index';
+import thunk from 'redux-thunk';
+// import  AsyncMiddleware  from '../middlewares/async'
+
+const store = createStore(
+    rootReducer, 
+    applyMiddleware(thunk), 
+    // applyMiddleware(AsyncMiddleware)
+);
+
+export default store;
