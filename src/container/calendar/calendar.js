91d58b5439af624d3be55525a1ce0945491f454b
Prabeen04 <prabeen.strange@gmail.com>
2018-03-30 21:15:08 +0530

    material Dialog added to calendar

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
 import moment from 'moment';
 import { myEventList } from './myEventList';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -7,14 +15,54 @@ BigCalendar.momentLocalizer(moment);
 class Calendar extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      open: false
+    }
     this.handleSelectSlot = this.handleSelectSlot.bind(this);
   }
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
   handleSelectSlot =(slot) => {
     console.log(slot.start)
   }  
   render() {
     return (
       <div style={{height: '520px'}}>
+      <Button onClick={this.handleClickOpen}>Open form dialog</Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address here. We will send
+              updates occationally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
        <BigCalendar
           events={myEventList}
           popup={true}
