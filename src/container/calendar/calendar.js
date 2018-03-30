647e89aaece071309cd6e96d75561758c8a5852b
Prabeen04 <prabeen.strange@gmail.com>
2018-03-30 20:18:47 +0530

    calendar props added

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import { myEventList } from './myEventList';
-
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 BigCalendar.momentLocalizer(moment);
 class Calendar extends Component {
   render() {
     return (
-      <div>
+      <div style={{height: '520px'}}>
        <BigCalendar
           events={myEventList}
           startAccessor='startDate'
           endAccessor='endDate'
+          selectable={true}
+          onSelecting={(range) => console.log(range)}
         />
       </div>
     )
