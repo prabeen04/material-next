8d298cadb3db2b27f82683f567fc3c3153ade408
Prabeen04 <prabeen.strange@gmail.com>
2018-03-30 21:09:41 +0530

    selectSlotHandler added

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -5,15 +5,24 @@ import { myEventList } from './myEventList';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 BigCalendar.momentLocalizer(moment);
 class Calendar extends Component {
+  constructor(props){
+    super(props)
+    this.handleSelectSlot = this.handleSelectSlot.bind(this);
+  }
+  handleSelectSlot =(slot) => {
+    console.log(slot.start)
+  }  
   render() {
     return (
       <div style={{height: '520px'}}>
        <BigCalendar
           events={myEventList}
+          popup={true}
           startAccessor='startDate'
           endAccessor='endDate'
           selectable={true}
           onSelecting={(range) => console.log(range)}
+          onSelectSlot={(slotInfo) => this.handleSelectSlot(slotInfo)}
         />
       </div>
     )
